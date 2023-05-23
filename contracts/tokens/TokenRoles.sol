fe81d760a614a6229c2f8ba41f280df26191769f
b0gdaniy <57547705+r0ugeEngine@users.noreply.github.com>
2023-05-23 15:06:12 +0300

    add: comments for this contract

--- contracts/tokens/TokenRoles.sol
+++ contracts/tokens/TokenRoles.sol
@@ -1,7 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+/**
+ * @title Abstract contract for tokens' roles
+ * @author Bohdan
+ */
 abstract contract TokenRoles {
+    /// @dev Keccak256 hashed `MINTER_ROLE` string
     bytes32 public constant MINTER_ROLE = keccak256(bytes("MINTER_ROLE"));
+    /// @dev Keccak256 hashed `BURNER_ROLE` string
     bytes32 public constant BURNER_ROLE = keccak256(bytes("BURNER_ROLE"));
 }
