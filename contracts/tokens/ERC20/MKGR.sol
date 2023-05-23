0b87feb4be8d02d5e5ecbf977a8c7cd5239d7cb0
b0gdaniy <57547705+r0ugeEngine@users.noreply.github.com>
2023-05-23 15:00:11 +0300

    add: MKGR erc20 token contract

---
+++ contracts/tokens/ERC20/MKGR.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+import "../TokenRoles.sol";
+
+/**
+ * @title MKGR token contract
+ * Can be used as staking reward token, or rewards for Oracle makers.
+ * @author Bohdan
+ */
+contract MKGR is TokenRoles, ERC20, AccessControl {
+    /// @notice Constructor to initialize NFT token contract
+    /// @dev Grants each roles to `msg.sender`
+    /// @dev Sets `name` and `symbol` of ERC721 token
+    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(MINTER_ROLE, msg.sender);
+        _grantRole(BURNER_ROLE, msg.sender);
+    }
+
+    /// @dev Mints `to` address some `amount` of tokens
+    /// @param to address to mint
+    /// @param amount of tokens to mint
+    function mint(address to, uint256 amount) external onlyRole(MINTER_ROLE) {
+        _mint(to, amount);
+    }
+
+    /// @dev Burns `from` address some `amount` of tokens
+    /// @param from address to mint
+    /// @param amount of tokens to burn
+    function burn(address from, uint256 amount) external onlyRole(BURNER_ROLE) {
+        _burn(from, amount);
+    }
+
+    /// @inheritdoc AccessControl
+    function supportsInterface(bytes4 interfaceId) public view override(AccessControl) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+}
