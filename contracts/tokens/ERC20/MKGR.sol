bd1b0d5d69fe8d146ca8f103b5461898d28329b7
b0gdaniy <57547705+r0ugeEngine@users.noreply.github.com>
2023-05-23 15:24:52 +0300

    changes: Added specific information to contracts

--- contracts/tokens/ERC20/MKGR.sol
+++ contracts/tokens/ERC20/MKGR.sol
@@ -21,16 +21,26 @@ contract MKGR is TokenRoles, ERC20, AccessControl {
         _grantRole(BURNER_ROLE, msg.sender);
     }
 
-    /// @dev Mints `to` address some `amount` of tokens
-    /// @param to address to mint
-    /// @param amount of tokens to mint
+    /**
+     * @dev Mints `to` address some `amount` of tokens
+     * Requirements:
+     * - only `MINTER_ROLE`
+     *
+     * @param to address to mint
+     * @param amount of tokens to mint
+     */
     function mint(address to, uint256 amount) external onlyRole(MINTER_ROLE) {
         _mint(to, amount);
     }
 
-    /// @dev Burns `from` address some `amount` of tokens
-    /// @param from address to mint
-    /// @param amount of tokens to burn
+    /**
+     * @dev Burns `from` address some `amount` of tokens
+     * Requirements:
+     * - only `BURNER_ROLE`
+     *
+     * @param from address to mint
+     * @param amount of tokens to burn
+     */
     function burn(address from, uint256 amount) external onlyRole(BURNER_ROLE) {
         _burn(from, amount);
     }
