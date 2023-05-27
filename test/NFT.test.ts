41002f066320d20a169ff7744131cc6edd13d66b
b0gdaniy <57547705+r0ugeEngine@users.noreply.github.com>
2023-05-27 14:31:02 +0300

    change: tests

--- test/NFT.test.ts
+++ test/NFT.test.ts
@@ -8,13 +8,15 @@ import { NRGS } from '../typechain/contracts/tokens/ERC721/NRGS';
 
 describe(`Tokens`, function () {
 
-  const otherAccAddress = '0x1400a04079772bf421bf53f25da828c95d4fa8bb';
-  let admin_role: string, minter_role: string, burner_role: string;
+  let otherAccAddress: string;
+  let admin_role: string, minter_role: string, burner_role: string, staking_role: string;
   // We define a fixture to reuse the same setup in every test.
   // We use loadFixture to run this setup once, snapshot that state,
   // and reset Hardhat Network to that snapshot in every test.
   async function deployFixture() {
     const [deployer, otherAcc] = await ethers.getSigners();
+
+    otherAccAddress = otherAcc.address.toLowerCase();
 
     const MCGR: ContractFactory = await ethers.getContractFactory(`MCGR`);
     const mcgr: MCGR = await MCGR.deploy() as MCGR;
--- test/Staking.test.ts
+++ test/Staking.test.ts
@@ -9,13 +9,15 @@ import { NRGS } from "../typechain/contracts/tokens/ERC721/NRGS";
 
 describe("Staking", function () {
 
-  const otherAccAddress = '0x1400a04079772bf421bf53f25da828c95d4fa8bb';
+  let otherAccAddress: string;
   let admin_role: string, minter_role: string, burner_role: string, staking_role: string;
   // We define a fixture to reuse the same setup in every test.
   // We use loadFixture to run this setup once, snapshot that state,
   // and reset Hardhat Network to that snapshot in every test.
   async function deployFixture() {
     const [deployer, otherAcc] = await ethers.getSigners();
+
+    otherAccAddress = otherAcc.address.toLowerCase();
 
     const MCGR: ContractFactory = await ethers.getContractFactory("MCGR");
     const mcgr: MCGR = await MCGR.deploy() as MCGR;
@@ -63,6 +65,7 @@ describe("Staking", function () {
     expect(await nrgs.hasRole(admin_role, deployer.address)).to.be.true;
     expect(await nrgs.hasRole(minter_role, deployer.address)).to.be.true;
     expect(await nrgs.hasRole(burner_role, deployer.address)).to.be.true;
+    expect(await stakingReward.hasRole(admin_role, deployer.address)).to.be.true;
     expect(await stakingReward.hasRole(staking_role, deployer.address)).to.be.true;
 
     expect(await stakingReward.NRGS()).to.be.eq(nrgs.address);
@@ -248,30 +251,21 @@ describe("Staking", function () {
       await expect(stakingReward.sendRewards(addressZero, 10)).to.be.revertedWith(errorMsg);
       await expect(stakingReward.exitStaking(addressZero, 10)).to.be.revertedWith(errorMsg);
       await expect(stakingReward.updateRewards(addressZero, 10)).to.be.revertedWith(errorMsg);
+      await expect(stakingReward.changeMCGR(addressZero)).to.be.revertedWith(errorMsg);
+      await expect(stakingReward.changeNRGS(addressZero)).to.be.revertedWith(errorMsg);
     });
 
     it('Is Correct Owner', async () => {
       const { stakingReward, otherAcc, deployer, nrgs } = await loadFixture(deployFixture);
       await nrgs.mint(deployer.address, 10);
-      const errorMsg = "StakingReward: supplier is not the owner of this token";
+      const errorMsg1 = "StakingReward: supplier is not the owner of this token";
+      const errorMsg2 = "StakingReward: supplier is not entered with this token";
 
 
-      await expect(stakingReward.enterStaking(otherAcc.address, 10)).to.be.revertedWith(errorMsg);
-      await expect(stakingReward.sendRewards(otherAcc.address, 10)).to.be.revertedWith(errorMsg);
-      await expect(stakingReward.exitStaking(otherAcc.address, 10)).to.be.revertedWith(errorMsg);
-      await expect(stakingReward.updateRewards(otherAcc.address, 10)).to.be.revertedWith(errorMsg);
-    });
-
-    it('Is Correct Owner', async () => {
-      const { stakingReward, otherAcc, deployer, nrgs } = await loadFixture(deployFixture);
-      await nrgs.mint(deployer.address, 10);
-      const errorMsg = "StakingReward: supplier is not the owner of this token";
-
-
-      await expect(stakingReward.enterStaking(otherAcc.address, 10)).to.be.revertedWith(errorMsg);
-      await expect(stakingReward.sendRewards(otherAcc.address, 10)).to.be.revertedWith(errorMsg);
-      await expect(stakingReward.exitStaking(otherAcc.address, 10)).to.be.revertedWith(errorMsg);
-      await expect(stakingReward.updateRewards(otherAcc.address, 10)).to.be.revertedWith(errorMsg);
+      await expect(stakingReward.enterStaking(otherAcc.address, 10)).to.be.revertedWith(errorMsg1);
+      await expect(stakingReward.sendRewards(otherAcc.address, 10)).to.be.revertedWith(errorMsg1);
+      await expect(stakingReward.exitStaking(otherAcc.address, 10)).to.be.revertedWith(errorMsg2);
+      await expect(stakingReward.updateRewards(otherAcc.address, 10)).to.be.revertedWith(errorMsg1);
     });
 
     it('updateRewards requires', async () => {
