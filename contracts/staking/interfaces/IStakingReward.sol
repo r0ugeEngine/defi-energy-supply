645426faa048f7e0198e80f0711e32aff05fb5aa
b0gdaniy <57547705+r0ugeEngine@users.noreply.github.com>
2023-05-27 12:21:20 +0300

    add: interface for staking

---
+++ contracts/staking/interfaces/IStakingReward.sol
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+/**
+ * @title Interface for StakingReward contract
+ * @author Bohdan
+ */
+interface StakingReward {
+    struct Supplier {
+        uint256 updatedAt;
+        uint256 pendingReward;
+    }
+
+    /// @dev Address to supplier
+    //mapping(address => mapping(uint => Supplier)) public suppliers;
+
+    /**
+     * @notice Enters staking process.
+     * Requirements:
+     * - `msg.sender` must have STAKING_MANAGER_ROLE
+     * - `supplier` must not be address 0
+     * - `supplier` must have NRGS token
+     *
+     * @param supplier address
+     * @param tokenId uint256
+     * @return bool
+     */
+    function enterStaking(address supplier, uint256 tokenId) external returns (bool);
+
+    /**
+     * @notice Sends rewards to suppliers.
+     * Requirements:
+     * - `msg.sender` must have STAKING_MANAGER_ROLE
+     * - `supplier` must be in staking
+     *
+     * @param supplier address
+     * @param tokenId uint256
+     * @return bool
+     */
+    function sendRewards(address supplier, uint256 tokenId) external returns (bool);
+
+    /**
+     * @notice Exits staking.
+     * Requirements:
+     * - `msg.sender` must have STAKING_MANAGER_ROLE
+     * - `supplier` must be in staking
+     *
+     * @param supplier address
+     * @param tokenId uint256
+     * @return bool
+     */
+    function exitStaking(address supplier, uint256 tokenId) external returns (bool);
+
+    /**
+     * @notice Updates rewards for `supplier`.
+     * Requirements:
+     * - `supplier` must be in staking
+     *
+     * @param supplier address
+     * @param tokenId uint256
+     * @return Supplier memory
+     */
+    function updateRewards(address supplier, uint tokenId) external returns (Supplier memory);
+}
