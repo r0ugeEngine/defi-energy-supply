8dfc99a25d901e817c5a70fb7539d4da0e525891
b0gdaniy <57547705+r0ugeEngine@users.noreply.github.com>
2023-06-01 13:02:45 +0300

    add: transfer approve functions

--- contracts/tokens/ERC20/interfaces/IMCGR.sol
+++ contracts/tokens/ERC20/interfaces/IMCGR.sol
@@ -1,9 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/access/IAccessControl.sol";
-
 /**
  * @title IMCGR interface of MCGR contract
  * Can be used as staking reward token, or rewards for Oracle makers.
@@ -29,4 +26,38 @@ interface IMCGR {
      * @param amount of tokens to burn
      */
     function burn(address from, uint256 amount) external;
+
+    /**
+     * @dev Moves `amount` tokens from the caller's account to `to`.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transfer(address to, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Moves `amount` tokens from `from` to `to` using the
+     * allowance mechanism. `amount` is then deducted from the caller's
+     * allowance.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transferFrom(address from, address to, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Atomically increases the allowance granted to `spender` by the caller.
+     *
+     * This is an alternative to {approve} that can be used as a mitigation for
+     * problems described in {IERC20-approve}.
+     *
+     * Emits an {Approval} event indicating the updated allowance.
+     *
+     * Requirements:
+     *
+     * - `spender` cannot be the zero address.
+     */
+    function increaseAllowance(address spender, uint256 addedValue) external returns (bool);
 }
