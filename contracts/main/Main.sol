87eb2b0cfb7da2a795040d6541c04f61269d36dd
b0gdaniy <57547705+r0ugeEngine@users.noreply.github.com>
2023-06-04 15:58:56 +0300

    add: NatSpec comments

--- contracts/main/Main.sol
+++ contracts/main/Main.sol
@@ -7,7 +7,7 @@ import "../manager/interfaces/IManager.sol";
 
 /**
  * @title Main
- * @dev A main contract for managing Microgrid ecosytem.
+ * @dev A main contract for managing Microgrid ecosystem.
  * @author Bohdan
  */
 contract Main is AccessControl {
@@ -19,18 +19,18 @@ contract Main is AccessControl {
 
     /// @dev Throws if passed address 0 as parameter
     modifier zeroAddressCheck(address account) {
-        require(account != address(0), "Escrow: account is address 0");
+        require(account != address(0), "Main: zero address");
         _;
     }
 
     /// @dev Throws if passed value is <= 0
     modifier gtZero(uint256 value) {
-        require(value > 0, "Escrow: passed value is <= 0");
+        require(value > 0, "Main: value is <= 0");
         _;
     }
 
     /**
-     * @notice Constructor to initialize the Escrow contract
+     * @notice Constructor to initialize the Main contract.
      * @param _manager The address of the Manager contract.
      * @dev Grants `DEFAULT_ADMIN_ROLE` and `MAIN_MANAGER_ROLE` roles to the contract deployer.
      */
@@ -40,26 +40,76 @@ contract Main is AccessControl {
         manager = _manager;
     }
 
+    /**
+     * @notice Registers an Energy supplier.
+     * Requirements:
+     * - `supplierId` must be greater than 0.
+     * - `buildingsNumber` must be greater than 0.
+     *
+     * @param supplierId The ID of the supplier.
+     * @param buildingsNumber The number of buildings for the supplier.
+     */
     function registerSupplier(uint256 supplierId, uint256 buildingsNumber) external {
         manager.register().registerSupplier(msg.sender, supplierId, buildingsNumber);
     }
 
+    /**
+     * @notice Registers an Electricity user.
+     * Requirements:
+     * - `supplierId` must be greater than 0.
+     * - `supplier` must not be address 0.
+     *
+     * @param supplierId The ID of the supplier.
+     * @param supplier The address of the supplier.
+     */
     function registerElectricityUser(uint256 supplierId, address supplier) external {
         manager.register().registerElectricityUser(msg.sender, supplierId, supplier);
     }
 
+    /**
+     * @notice Unregisters an Energy supplier.
+     * Requirements:
+     * - `supplierId` must be greater than 0.
+     *
+     * @param supplierId The ID of the supplier.
+     */
     function unRegisterSupplier(uint256 supplierId) external {
         manager.register().unRegisterSupplier(msg.sender, supplierId);
     }
 
+    /**
+     * @notice Unregisters an Electricity user.
+     * Requirements:
+     * - `supplierId` must be greater than 0.
+     *
+     * @param supplierId The ID of the supplier.
+     */
     function unRegisterElectricityUser(uint256 supplierId) external {
         manager.register().unRegisterElectricityUser(msg.sender, supplierId);
     }
 
+    /**
+     * @notice Pays for electricity.
+     * Requirements:
+     * - `supplierId` must be greater than 0.
+     * - `supplier` must not be address 0.
+     * - `amountToPay` must be greater than 0.
+     *
+     * @param supplierId The ID of the supplier.
+     * @param supplier The address of the supplier.
+     * @param amountToPay The amount to pay for electricity.
+     */
     function payForElectricity(uint256 supplierId, address supplier, uint256 amountToPay) external {
         manager.escrow().sendFundsToSupplier(msg.sender, supplierId, supplier, amountToPay);
     }
 
+    /**
+     * @notice Gets the rewards for a supplier.
+     * Requirements:
+     * - `supplierId` must be greater than 0.
+     *
+     * @param supplierId The ID of the supplier.
+     */
     function getRewards(uint256 supplierId) external {
         manager.staking().sendRewards(msg.sender, supplierId);
     }
